f4ee19b6cb67c93d453bf8721dad3005d5142da7
Cristóbal Castillo <castillo85@gmail.com>
2014-11-25 21:37:09 -0300

    use custom styles (no bootstrap)

--- app/helpers/choices_helper.rb
+++ app/helpers/choices_helper.rb
@@ -2,7 +2,7 @@ module ChoicesHelper
 
   def display_percent(choice)
     display_color = {'0' => 'red', '25' => 'orange', '50' => 'yellow', '75' => 'green', '100' => 'green'}
-    image_tag "#{choice.name}#{display_color[choice.name]}45px.png"
+    "background-image: url(/assets/#{choice.name}#{display_color[choice.name]}45px.png)"
   end
 
 end
