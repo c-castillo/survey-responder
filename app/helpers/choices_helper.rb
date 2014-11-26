7df69aa492e5b80b8d882ebf26eed0eb47417cf2
Cristóbal Castillo <ccastillo@comparaonline.com>
2014-11-26 16:06:10 -0300

    modify the helper to return dynamic image path

--- app/helpers/choices_helper.rb
+++ app/helpers/choices_helper.rb
@@ -2,7 +2,7 @@ module ChoicesHelper
 
   def display_percent(choice)
     display_color = {'0' => 'red', '25' => 'orange', '50' => 'yellow', '75' => 'green', '100' => 'green'}
-    "background-image: url(/assets/#{choice.name}#{display_color[choice.name]}45px.png)"
+    "background-image: url('#{path_to_image("#{choice.name}#{display_color[choice.name]}45px.png")}')"
   end
 
 end
